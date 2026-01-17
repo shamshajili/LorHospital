61cc747e4be335115f0d430f955740145d83b889
shamshajili <shamshacili@gmail.com>
2026-01-18 01:19:22 +0400

    added footer and fix problems

--- App.jsx
+++ App.jsx
@@ -1,27 +1,28 @@
-
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Topbar from './components/layout/Topbar'
 import Navbar from './components/layout/Navbar'
-import Hero from './components/home/Hero'
-import Features from './components/home/Features'
-import DoctorsCarousel from './components/home/DoctorsCarousel'
-import ArticlesCarousel from './components/home/ArticlesCarousel'
-import WhyLorHospital from './components/home/WhyLorHospital'
-import ReviewsSection from './components/home/ReviewsSection'
+import Footer from './components/layout/Footer'
+
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import DoctorDetail from './pages/DoctorDetail'
 
 function App() {
-
   return (
-   <div>
-    <Topbar/>
-    <Navbar/>
-    <Hero/>
-    <Features/>
-    <DoctorsCarousel/>
-    <ArticlesCarousel/>
-    <WhyLorHospital/>
-    <ReviewsSection/>
-   </div>
+    <BrowserRouter>
+      <Topbar />
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:id" element={<DoctorDetail />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   )
 }
 
